8dfe104841dd971a80bba13c374336b602e1b9c8
thigo47 <vilaportilho@gmail.com>
2023-03-26 19:24:10 +0100

    interaçao quase pronta

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 let saldoconta = document.getElementById("saldoconta")
 let quantosacar = document.getElementById("quantosacar")
+let quantodepositar = document.getElementById("quantodepositar")
+
+let his = []
 
 let saldo = 1000;
-saldoconta.innerHTML+= saldo
+let saldoeuro = saldo.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' });
+saldoconta.innerHTML+= saldoeuro
 
 let qtsacar = document.getElementById("saque")
 let snum = document.getElementById("numtxt")
+let qtdeposito = document.getElementById("qtdeposito")
+///////////////////////////////////////////////////////
+let novadivdep = document.createElement("div")
 let novadiv = document.createElement("div") // cria a div apenas uma vez
+let novadivdeposito = document.createElement("div")
 
 function sacar(params) {
     if (saldo == 0) {
@@ -36,17 +44,68 @@ function sacar(params) {
                     window.alert("Digite o valor desejado")
                 } else if(snum.value > saldo) {
                     window.alert("Valor invalido , Valor maior que saldo em conta")
-                } else {
-                    novadiv.innerHTML = `Você sacou ${snum.value}` // atualiza o valor da div
+                } else if(snum.value == 0) {
+                    window.alert("valor invalido")
+                }else{
+                    novadiv.innerHTML += `Você sacou ${snum.value} € <br>` // atualiza o valor da div
                     quantosacar.appendChild(novadiv)
 
-                    let novosaldosaque = saldo - snum.value
-                    saldo = novosaldosaque
-                    saldoconta.innerHTML = `Saldo da conta : ${novosaldosaque}`
+                    
+                    saldo -= snum.value
+                    saldoconta.innerHTML = `Saldo da conta : ${saldo.toLocaleString('de-DE', { style: 'currency', currency: 'EUR'})}`
                 }
             }
             saque.appendChild(saqueagr)
         }
     }
 }
-//aparentemente saldo esta completo
+//aparentemente saque esta completo
+
+//fazere um historico de saque com for
+
+function depositar(params) {
+    
+
+        if (!qtdeposito) {
+            qtdeposito = document.createElement("p")
+            qtdeposito.id = "qtdeposito"
+        qtdeposito.innerHTML = `Quando voce deseja Depositar `
+
+        quantodepositar.appendChild(qtdeposito)
+
+        let numd = document.createElement("input")
+        numd.type = "number"
+        numd.id = "numdep"
+
+        qtdeposito.appendChild(numd)
+
+         let depositeagr = document.createElement("button")
+         depositeagr.innerHTML = "Depositar"
+
+         let np = document.getElementById("numdep")
+
+         if (np == 0) {
+            window.alert("Valor invalido")
+         }else if ( np.length == 0){
+            window.alert("Valor invalido , por favor insira um valor valido")
+         }else{
+            depositeagr.onclick = ()=>{
+                saldo =  saldo + Number(np.value)
+                saldoconta.innerHTML = `Saldo da conta : ${saldo.toLocaleString('de-DE', { style: 'currency', currency: 'EUR'})}`
+
+               novadivdeposito.innerHTML = `Você depositou ${np.value} €`
+
+                his.push(`Valor depositado${np.value}`)
+
+                window.alert(his)
+
+               qtdeposito.appendChild(novadivdeposito)
+             }
+             qtdeposito.appendChild(depositeagr)           
+         }         
+        }
+}
+
+function historico() {
+    
+}
