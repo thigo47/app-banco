4f8b32b757603f51685c57f48e9995c3b48e3066
thigo47 <vilaportilho@gmail.com>
2023-04-05 22:58:17 +0100

    cadastro sendo atualizado

---
+++ cadastro/script.js
@@ -0,0 +1,15 @@
+    let i = {
+    nome:document.getElementById("nome"),    
+    endereço:document.getElementById("endereço"),
+    tel:document.getElementById("tel"),
+    foto:document.getElementById("foto"),
+    email:document.getElementById("email"),
+    senha:document.getElementById("senha"),
+    res:document.getElementById("resp"),
+    cadastro:document.getElementById("cadastro")
+}
+
+i.cadastro.addEventListener("click", function(){
+    window.alert("")
+    
+})
